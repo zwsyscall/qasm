d6d2b81a93517a4fd9eacae3b4b9d4ed1ca3399a
zwsyscall <61552486+zwsyscall@users.noreply.github.com>
2026-02-26 19:18:51 +0200

    Removed hex .rs& checks.rs

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -1,4 +1,28 @@
 pub mod assembler;
-pub mod checks;
 pub mod disassmbler;
-pub mod hex;
+pub mod oneshot;
+
+pub enum InputType {
+    Hex,
+    Assembly,
+}
+
+pub fn identify_type(input: &[&str]) -> InputType {
+    let full_text = input.join("");
+    if full_text.trim().is_empty() || full_text.contains(":") {
+        return InputType::Assembly;
+    }
+
+    let clean_text = full_text
+        .replace("0x", "")
+        .replace(" ", "")
+        .replace(",", "")
+        .replace(";", "")
+        .replace("\n", "");
+
+    if hex::decode(&clean_text).is_ok() {
+        InputType::Hex
+    } else {
+        InputType::Assembly
+    }
+}
