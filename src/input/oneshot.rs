d74ff9a7c37ea7b76ef9fff52642fcb5fc272862
zwsyscall <61552486+zwsyscall@users.noreply.github.com>
2026-02-26 19:18:36 +0200

    Move oneshot away from main

---
+++ src/input/oneshot.rs
@@ -0,0 +1,78 @@
+use std::fs;
+
+use crate::output::{AsmSyntax, format_bytes};
+use crate::{Cli, input};
+use capstone::Capstone;
+use capstone::arch::{BuildsCapstone, BuildsCapstoneSyntax};
+use keystone_engine::{Keystone, Mode, OptionType, OptionValue};
+
+use crate::input::InputType;
+use crate::input::assembler::assemble_text;
+use crate::input::disassmbler::disassemble_text;
+
+pub fn analyze(cli: Cli, mut text_vec: String) -> String {
+    // Setup
+    let (ks_mode, cs_mode) = match cli.mode {
+        32 | 86 => (Mode::MODE_32, capstone::arch::x86::ArchMode::Mode32),
+        64 => (Mode::MODE_64, capstone::arch::x86::ArchMode::Mode64),
+        _ => panic!("Unsupported mode. Use 32 or 64."),
+    };
+    let (cs_syntax, ks_syntax) = match cli.syntax {
+        AsmSyntax::Intel => (
+            capstone::arch::x86::ArchSyntax::Intel,
+            OptionValue::SYNTAX_INTEL,
+        ),
+        AsmSyntax::Att => (
+            capstone::arch::x86::ArchSyntax::Att,
+            OptionValue::SYNTAX_ATT,
+        ),
+    };
+
+    let ks = Keystone::new(keystone_engine::Arch::X86, ks_mode)
+        .expect("Failed creating keystone object");
+    ks.option(OptionType::SYNTAX, ks_syntax)
+        .expect("Failed setting keystone syntax");
+    let cs = Capstone::new()
+        .x86()
+        .mode(cs_mode)
+        .syntax(cs_syntax)
+        .build()
+        .expect("Failed creating capstone object");
+
+    // 'biz
+    if let Some(path) = cli.input {
+        let data = fs::read(path).expect("Unable to read file");
+        text_vec = hex::encode(data);
+    }
+
+    match input::identify_type(&[text_vec.as_str()]) {
+        InputType::Hex => {
+            if let Some((output_lines, _size)) =
+                disassemble_text(&cs, vec![text_vec.as_str()], 0x0, true)
+            {
+                return output_lines.join("\n");
+            } else {
+                return "Failed decoding hex".to_string();
+            }
+        }
+        InputType::Assembly => {
+            if let Some((mapped_bytes, _size)) =
+                assemble_text(&ks, &cs, vec![text_vec.as_str()], 0x0)
+            {
+                let output_lines: Vec<String> = mapped_bytes
+                    .into_iter()
+                    .map(|line_bytes| {
+                        if line_bytes.is_empty() {
+                            String::new()
+                        } else {
+                            format_bytes(&line_bytes, &cli.format)
+                        }
+                    })
+                    .collect();
+                return output_lines.join("\n");
+            } else {
+                return "Failed assembling code".to_string();
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,113 +1,59 @@
+use crate::{
+    input::oneshot,
+    output::{AsmSyntax, HexFormat},
+};
 use clap::Parser;
-use std::io;
-
-use capstone::Capstone;
-use capstone::arch::{BuildsCapstone, BuildsCapstoneSyntax};
-use keystone_engine::{Keystone, Mode, OptionType, OptionValue};
-
-use crate::input::assembler::assemble_text;
-use crate::input::checks::InputType;
-use crate::input::disassmbler::disassemble_text;
-use crate::output::{AsmSyntax, HexFormat, format_bytes};
+use std::{io, path::PathBuf};
 
 #[derive(Parser, Debug)]
 #[command(name = "qasm")]
 #[command(version, about = "A quick assembler/disassembler", long_about = None)]
-struct Cli {
+pub struct Cli {
     /// Engine architecture mode (32 / 86, 64)
     #[arg(short, long, default_value_t = 64)]
-    mode: u8,
+    pub mode: u8,
 
     /// Syntax  (intel, att)
     #[arg(short, long, value_enum, default_value_t = AsmSyntax::Intel)]
-    syntax: AsmSyntax,
+    pub syntax: AsmSyntax,
 
     /// Output formatting to use
     #[arg(short, long, value_enum, default_value_t = HexFormat::Pretty)]
-    format: HexFormat,
+    pub format: HexFormat,
 
     /// Address to use for disassembly
     #[arg(short, long, default_value_t = 0x0)]
-    address: u64,
+    pub address: u64,
+
+    /// Parse file as input
+    #[arg(short, long)]
+    pub input: Option<PathBuf>,
 
     /// The raw input data to evaluate (e.g., 0xc3 or "mov eax, 1")
     #[arg(allow_hyphen_values = true)]
-    raw_data: Vec<String>,
+    pub data: Vec<String>,
 }
 
 mod gui;
 mod input;
 mod output;
+mod worker;
 
 fn main() -> io::Result<()> {
     let cli = Cli::parse();
-
-    let (ks_mode, cs_mode) = match cli.mode {
-        32 | 86 => (Mode::MODE_32, capstone::arch::x86::ArchMode::Mode32),
-        64 => (Mode::MODE_64, capstone::arch::x86::ArchMode::Mode64),
-        _ => panic!("Unsupported mode. Use 32 or 64."),
+    let gui_syntax = match cli.syntax {
+        AsmSyntax::Intel => output::AsmSyntax::Intel,
+        AsmSyntax::Att => output::AsmSyntax::Att,
     };
-
-    let (cs_syntax, ks_syntax, gui_syntax) = match cli.syntax {
-        AsmSyntax::Intel => (
-            capstone::arch::x86::ArchSyntax::Intel,
-            OptionValue::SYNTAX_INTEL,
-            output::AsmSyntax::Intel,
-        ),
-        AsmSyntax::Att => (
-            capstone::arch::x86::ArchSyntax::Att,
-            OptionValue::SYNTAX_ATT,
-            output::AsmSyntax::Att,
-        ),
-    };
-
-    let ks = Keystone::new(keystone_engine::Arch::X86, ks_mode)
-        .expect("Failed creating keystone object");
-
-    ks.option(OptionType::SYNTAX, ks_syntax)
-        .expect("Failed setting keystone syntax");
-
-    let cs = Capstone::new()
-        .x86()
-        .mode(cs_mode)
-        .syntax(cs_syntax)
-        .build()
-        .expect("Failed creating capstone object");
 
     // One-off
-    let raw_input = cli.raw_data.join(" ");
-    if !raw_input.trim().is_empty() {
-        let text_vec = vec![raw_input.as_str()];
-
-        match input::checks::identify_type(&text_vec) {
-            InputType::Hex => {
-                if let Some(output_lines) = disassemble_text(&cs, text_vec, 0x0, false) {
-                    println!("{}", output_lines.join("\n"))
-                } else {
-                    println!("Failed decoding hex");
-                }
-            }
-            InputType::Assembly => {
-                if let Some(mapped_bytes) = assemble_text(&ks, &cs, text_vec, 0x0) {
-                    let output_lines: Vec<String> = mapped_bytes
-                        .into_iter()
-                        .map(|line_bytes| {
-                            if line_bytes.is_empty() {
-                                String::new()
-                            } else {
-                                format_bytes(&line_bytes, &cli.format)
-                            }
-                        })
-                        .collect();
-                    println!("{}", output_lines.join("\n"))
-                } else {
-                    println!("Failed assembling code");
-                }
-            }
-        }
+    let raw_input = cli.data.join(" ");
+    if !raw_input.trim().is_empty() || cli.input.is_some() {
+        let output_text = oneshot::analyze(cli, raw_input);
+        println!("{}", output_text);
     } else {
         // TUI
-        gui::run(cs, ks, cli.mode, gui_syntax)?;
+        gui::run(cli.mode, gui_syntax)?;
     }
     Ok(())
 }
